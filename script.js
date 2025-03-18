f6ab76dcb6a0e08c4a935533bec0fd361bbc7d49
VictorLpr <victorlepron@gmail.com>
2025-03-18 15:46:20 +0100

    continent OK

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const mapArea = document.getElementById("map")
 let allLaureates = [];
+let count = {
+    continent: {},
+    country: {}
+}
+let continentPos = {
+    Africa: {
+        latitude:7.4,
+        longitude:20
+    },
+    Asia : {
+        latitude: 51.8,
+        longitude: 82.7
+    },
+    Europe: {
+        latitude: 48,
+        longitude: 9
+    },
+    NorthAmerica: {
+        latitude: 33.5,
+        longitude: -98.8
+    },
+    SouthAmerica: {
+        latitude: -13.9,
+        longitude: -56.9
+    },
+    Oceania: {
+        latitude: -26,
+        longitude: 136.5
+    }
+}
 
 //création de la carte
 var map = L.map('map').setView([0, 0], 1);
@@ -7,17 +37,20 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+let markerGroup = L.layerGroup().addTo(map)
+let continentGroup = L.layerGroup().addTo(map)
 
 //chargement des prix nobels
 async function loadNoblePrizes(url) {
     const res = await fetch(url);
     const data = await res.json();
     allLaureates.push(...data.laureates)
+    countCoutryContinent();
     displayContinent(allLaureates);
 }
 
-let markerGroup = L.layerGroup().addTo(map)
 function displayMarkers(laureates) {
+    continentGroup.clearLayers();
     laureates.forEach(laureate => {
         console.log(laureate)
         // console.log(laureate.birth.place.cityNow.latitude)
@@ -32,40 +65,49 @@ function displayMarkers(laureates) {
     });
 
 }
-function displayContinent(laureates) {
-    let asia = 0;
-    let eu = 0;
-    let africa = 0;
-    let na = 0;
-    let sa = 0;
-    let oceania = 0;
-    markerGroup.clearLayers();
-    allLaureates.forEach(laureate => {
-        if (laureate.birth?.place?.continent) {
 
-            if (laureate.birth.place.continent = "North America") na++;
-            if (laureate.birth.place.continent = "Oceania") oceania++;
-            if (laureate.birth.place.continent = "Europe") eu++;
-            if (laureate.birth.place.continent = "South America") sa++;
-            if (laureate.birth.place.continent = "Asia") asia++;
-            if (laureate.birth.place.continent = "Africa") africa++;
+function countCoutryContinent () {
+    allLaureates.forEach(laureate => {
+        if(laureate.birth?.place?.continent) {
+            if (count.continent[`${(laureate.birth?.place?.continent.en).split(" ").join("")}`] === undefined) {
+                count.continent[`${(laureate.birth?.place?.continent.en).split(" ").join("")}`] = 1;
+                console.log(count)
+            } else {
+                count.continent[`${(laureate.birth?.place?.continent.en).split(" ").join("")}`]++;
+            }
+        }
+        if(laureate.birth?.place?.country) {
+            if (count.country[`${laureate.birth?.place?.country.en}`] === undefined) {
+                count.country[`${laureate.birth?.place?.country.en}`] = 1;
+                console.log(count)
+            } else {
+                count.country[`${laureate.birth?.place?.country.en}`]++;
+            }
         }
     })
-    console.log(eu)
-    var circle = L.circle([48, 9], {
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5,
-        radius: 500000
-    }).addTo(map);
-    var marker = L.marker([48, 9], {
-        icon: L.divIcon({
-            className: 'cicle-label',
-            html: `${eu}`,
-            iconSize: [50,50],
-            iconAnchor: [10,10]
-        })
-    }).addTo(map)
+
+}
+
+function displayContinent(laureates) {
+    markerGroup.clearLayers();
+    for (const cont in count.continent) {
+        console.log(cont)
+        var circle = L.circle([continentPos[`${cont}`].latitude, continentPos[`${cont}`].longitude], {
+            color: 'blue',
+            fillColor: 'blue',
+            fillOpacity: 0.5,
+            radius: 500000
+        }).addTo(continentGroup);
+        var marker = L.marker([continentPos[`${cont}`].latitude, continentPos[`${cont}`].longitude], {
+            icon: L.divIcon({
+                className: 'cicle-label',
+                html: `${count.continent[cont]}`,
+                iconSize: [50,50],
+                iconAnchor: [10,10]
+            })
+        }).addTo(continentGroup)
+       
+    }
 }
 loadNoblePrizes('https://api.nobelprize.org/2.1/laureates?offset=0&limit=500');
 
